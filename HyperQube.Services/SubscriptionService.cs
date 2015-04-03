e05021c95605ed9a2ae471ef14f3769fd6b210e9
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-03 18:37:49 +0200

    Update LICENSE

--- HyperQube.Services/SubscriptionService.cs
+++ HyperQube.Services/SubscriptionService.cs
@@ -1,21 +1,3 @@
-#region License
-
-//  Copyright 2014 Steven Thuriot
-//   
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//  
-//  http://www.apache.org/licenses/LICENSE-2.0
-//  
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
-#endregion
-
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -25,10 +7,10 @@ using System.Linq;
 using System.Net;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using HyperQube.Library.Extensions;
-using Newtonsoft.Json;
 using HyperQube.Library;
+using HyperQube.Library.Extensions;
 using HyperQube.Library.Questions;
+using Newtonsoft.Json;
 using WebSocketSharp;
 
 namespace HyperQube.Services
